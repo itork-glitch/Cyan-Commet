c262bb8809636a9de99c2e059b5785c8b60e72a9
itork-glitch <piter09102009sx@gmail.com>
2023-01-31 18:26:21 +0100

    Add commet

---
+++ src/config.hpp
@@ -0,0 +1,5 @@
+#define LED_PIN 6
+#define LED_COUNT 60
+#define BRIGHTNESS 255
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
 #include <Adafruit_NeoPixel.h>
-
-#define LED_PIN 6
-#define LED_COUNT 60
-#define BRIGHTNESS 255
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+#include <config.hpp>
 
 void setup()
 {
